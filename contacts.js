d9f6e048d634865f3ae88a37039c5c739b49966c
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-30 19:59:12 +0100

    add logic

--- contacts.js
+++ contacts.js
@@ -1,1 +1,54 @@
 import fs from 'fs';
+import path from 'path';
+import { nanoid } from 'nanoid';
+import { stringify } from 'querystring';
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+const readFile = async () => {
+  const data = await fs.readFileSync(contactsPath, 'utf8');
+  return JSON.parse(data);
+};
+
+const writeFile = async (data) => {
+  await fs.writeFileSync(contactsPath, JSON.stringify(data), 'utf8');
+};
+
+export async function listContacts() {
+  const data = await readFile();
+  console.table(data);
+}
+
+export async function getContactById(contactId) {
+  const data = await readFile();
+
+  const foundId = data.filter(({ id }) => id === contactId);
+  console.table(foundId);
+}
+
+export async function removeContact(contactId) {
+  const data = await readFile();
+
+  const foundContactIdx = data.findIndex(({ id }) => id === contactId);
+  const newData = data.splice(foundContactIdx, 1);
+  await writeFile(newData);
+
+  console.table(newData);
+  return true;
+}
+
+export async function addContact(name, email, phone) {
+  const data = await readFile();
+
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  data.push(newContact);
+  await writeFile(data);
+  console.table(newContact);
+
+  return data;
+}
