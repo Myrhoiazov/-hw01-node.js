a9889fb8d6a59ac4ef96fcb65cf6cabfc83aafec
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-10-31 11:24:52 +0100

    fixed async

--- contacts.js
+++ contacts.js
@@ -5,48 +5,72 @@ import { nanoid } from 'nanoid';
 const contactsPath = path.resolve('./db/contacts.json');
 
 const readFile = async () => {
-  const data = await fs.readFileSync(contactsPath, 'utf8');
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFileSync(contactsPath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const writeFile = async (data) => {
-  await fs.writeFileSync(contactsPath, JSON.stringify(data), 'utf8');
+  try {
+    await fs.writeFileSync(contactsPath, JSON.stringify(data), 'utf8');
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export async function listContacts() {
-  const data = await readFile();
-  console.table(data);
+  try {
+    const data = await readFile();
+    console.table(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function getContactById(contactId) {
-  const data = await readFile();
+  try {
+    const data = await readFile();
 
-  const foundId = data.filter(({ id }) => id === contactId);
-  console.table(foundId);
+    const foundId = data.filter(({ id }) => id === contactId);
+    console.table(foundId);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function removeContact(contactId) {
-  const data = await readFile();
+  try {
+    const data = await readFile();
 
-  const foundContactIdx = data.findIndex(({ id }) => id === contactId);
-  const newData = data.splice(foundContactIdx, 1);
-  await writeFile(newData);
-  console.table(newData);
-  return true;
+    const foundContactIdx = data.findIndex(({ id }) => id === contactId);
+    const newData = data.splice(foundContactIdx, 1);
+    await writeFile(data);
+    console.table(newData);
+    return true;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function addContact(name, email, phone) {
-  const data = await readFile();
+  try {
+    const data = await readFile();
 
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  data.push(newContact);
-  await writeFile(data);
-  console.table(newContact);
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    data.push(newContact);
+    await writeFile(data);
+    console.table(newContact);
 
-  return data;
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
